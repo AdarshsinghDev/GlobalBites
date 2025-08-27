7580470bfe56f6ffc996328eab1d46819be1ac44
AdarshsinghDev <mrideal000@gmail.com>
2025-08-27 22:35:31 +0530

    smtp updated

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,10 +2,9 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 export const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  service: "gmail",
-  port: 465,
-  secure: true,
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
